bb7f278383dc115d8e24fcb97e00db9c7fcc91e6
zcyue <973294528@qq.com>
2020-09-05 22:52:16 +0800

    add toolbar

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -25,11 +25,25 @@
     //     document.body.appendChild(view.build());
     // });
 
+    // maria.on(window, 'load', function() {
+
+    //     var model = new checkit.TodosModel();
+    
+    //     var view = new checkit.TodosInputView(model);
+    //     document.body.appendChild(view.build());
+    
+    //     var view = new checkit.TodosListView(model);
+    //     document.body.appendChild(view.build());
+    // });
+
     maria.on(window, 'load', function() {
 
         var model = new checkit.TodosModel();
     
         var view = new checkit.TodosInputView(model);
+        document.body.appendChild(view.build());
+    
+        var view = new checkit.TodosToolbarView(model);
         document.body.appendChild(view.build());
     
         var view = new checkit.TodosListView(model);
